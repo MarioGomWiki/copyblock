6ce5834c94d887eb64b6594bb426a8cdfd3bc424
MarioGom <MarioGomWiki@users.noreply.github.com>
2023-08-23 22:05:17 +0200

    limit p2p proxy blocks to ST47ProxyBot

--- blockbot-es.sh
+++ blockbot-es.sh
@@ -2,7 +2,7 @@
 
 # P2P proxies (bot blocks)
 
-python copyblock.py --comment-pattern '(?i)^{{blocked p2p proxy' --block-reason "{{Proxy P2P bloqueado}}. Visita [[Wikipedia:Proxies abiertos|la página informativa]] si estás afectado. <!-- Proxy P2P confirmado por API -->" --really-run
+python copyblock.py --source-user ST47ProxyBot --comment-pattern '(?i)^{{blocked p2p proxy' --block-reason "{{Proxy P2P bloqueado}}. Visita [[Wikipedia:Proxies abiertos|la página informativa]] si estás afectado. <!-- Proxy P2P confirmado por API -->" --really-run
 
 # Regular proxies
 
