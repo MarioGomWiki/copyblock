8ec17f6ae4062a21b8c3fbda964e34be48089a54
MarioGom <MarioGomWiki@users.noreply.github.com>
2023-09-04 23:33:30 +0200

    simple quotes in sh

--- blockbot-es.sh
+++ blockbot-es.sh
@@ -2,13 +2,13 @@
 
 # P2P proxies (bot blocks)
 
-python copyblock.py --source-user ST47ProxyBot --comment-pattern '(?i)^{{blocked p2p proxy' --block-reason "{{Proxy P2P bloqueado}}. Visita [[Wikipedia:Proxies abiertos|la página informativa]] si estás afectado. <!-- Proxy P2P confirmado por API -->" --really-run
+python copyblock.py --source-user ST47ProxyBot --comment-pattern '(?i)^{{blocked p2p proxy' --block-reason '{{Proxy P2P bloqueado}}. Visita [[Wikipedia:Proxies abiertos|la página informativa]] si estás afectado. <!-- Proxy P2P confirmado por API -->' --really-run
 
 # Regular proxies
 
-python copyyblock.py --comment-pattern '(?i)^{{blocked ?proxy' --block-reason "{{proxy bloqueado}}. Visita [[Wikipedia:Proxies abiertos|la página informativa]] si estás afectado. <!-- Bloqueo hecho por bot -->" --really-run
-python copyblock.py --comment-pattern '(?i)^{{blocked ?proxy' --ranges --block-reason "{{proxy bloqueado}}. Visita [[Wikipedia:Proxies abiertos|la página informativa]] si estás afectado. <!-- Bloqueo hecho por bot -->" --really-run
+python copyyblock.py --comment-pattern '(?i)^{{blocked ?proxy' --block-reason '{{proxy bloqueado}}. Visita [[Wikipedia:Proxies abiertos|la página informativa]] si estás afectado. <!-- Bloqueo hecho por bot -->' --really-run
+python copyblock.py --comment-pattern '(?i)^{{blocked ?proxy' --ranges --block-reason '{{proxy bloqueado}}. Visita [[Wikipedia:Proxies abiertos|la página informativa]] si estás afectado. <!-- Bloqueo hecho por bot -->' --really-run
 
 # Webhosts
 
-python copyblock.py --comment-pattern '(?i)^{{(?:colocation)?webhost' --ranges --block-reason "{{Webhost bloqueado}}. Visita [[Wikipedia:Proxies abiertos|la página informativa]] si estás afectado. <!-- Bloqueo hecho por bot -->" --really-run
+python copyblock.py --comment-pattern '(?i)^{{(?:colocation)?webhost' --ranges --block-reason '{{Webhost bloqueado}}. Visita [[Wikipedia:Proxies abiertos|la página informativa]] si estás afectado. <!-- Bloqueo hecho por bot -->' --really-run
